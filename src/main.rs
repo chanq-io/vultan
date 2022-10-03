e3b25c039faf239e724de478f21f635e374bdd5f
Pierre Chanquion <pierre@chanq.io>
2022-10-03 13:26:32 +0100

    update other .default() impls to be impls of Default trait

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-use vultan::application_state::card::parser::{Parser, ParsingConfig, ParsingPattern};
+use vultan::application_state::card::parser::{Parser, ParsingConfig};
 use vultan::application_state::card::Card;
 use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
-    let config = ParsingConfig {
-        tags_pattern: ParsingPattern::TaggedLine {
-            tag: String::from(r"tags:"),
-        },
-        tag_delimiter: String::from(":"),
-        question_pattern: ParsingPattern::WrappedMultiLine {
-            opening_tag: String::from(r"# Question"),
-            closing_tag: String::from(r"# Answer"),
-        },
-        answer_pattern: ParsingPattern::WrappedMultiLine {
-            opening_tag: String::from(r"# Answer"),
-            closing_tag: String::from(r"----\n"),
-        },
-    };
+    let config = ParsingConfig::default();
     let parser = Parser::from(config)?;
     println!("{:?}", Card::from("./test_card.md", &parser));
     Ok(())
